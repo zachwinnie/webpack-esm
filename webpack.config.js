5a6ff4ff95a4bde2f25561fbdc053f0de534ce02
Zach Winnie <zach.winnie@brandfolder.com>
2021-11-29 16:02:51 -0600

    webpack4 working

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+/**
+ * https://webpack.js.org/configuration/output/#module-definition-systems
+ * https://github.com/webpack/webpack/issues/2933#issuecomment-774253975
+ */
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  externals: {
+    'react': 'react',
+    'react-dom': 'react-dom'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.(ts|tsx)$/,
+        use: ['ts-loader']
+      }
+    ]
+  },
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    path: path.join(__dirname, '/lib')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
+};
